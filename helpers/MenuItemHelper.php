4b90fc8f16d8680b4cd3aba83a1b4c0fad0cb78b
Pascal Müller <pascal.mueller@asinfotrack.ch>
2018-04-01 10:56:04 +0200

    menu item helper completed with active paths and cache

--- helpers/MenuItemHelper.php
+++ helpers/MenuItemHelper.php
@@ -18,7 +18,7 @@ use asinfotrack\yii2\article\models\MenuItem;
 class MenuItemHelper
 {
 
-	protected $itemModels = [];
+	protected static $ITEM_CACHE;
 
 	/**
 	 * Converts the db models into menu items as used by the yii2-widgets.
@@ -28,16 +28,93 @@ class MenuItemHelper
 	 */
 	public static function getMenuItemsForNav(MenuItem $menuItem) : array
 	{
-		//fetch data
-		$allItems = $menuItem->children()->all();
+		//assert items are cached
+		static::cacheMenuItems();
 
 		//perform work
 		$targetArr = [];
-		$topLevelItems = static::getChildrenForItem($menuItem, $allItems);
-		foreach ($topLevelItems as $item) {
-			static::prepareItem($item, $targetArr, $allItems);
+		$items = static::getChildrenForItem($menuItem);
+		foreach ($items as $item) {
+			static::prepareItem($item, $targetArr,$items);
 		}
 		return $targetArr;
+	}
+
+	/**
+	 * Performs the activation of menu items according to a callback.
+	 * The callback should have the signature `function ($item, $parentItems)` and return a boolean value
+	 * whether or not the current item is active. The structure of `$item` is the same as the one used in the nav widget.
+	 *
+	 * @see \yii\widgets\Menu::$items
+	 *
+	 * @param array $items the item array to activate
+	 * @param callable $isActiveCallback the callback to determine if an item is active
+	 * @param bool $activateParents whether or not to activate parent items
+	 * @param array $parentItems the list of parent items of the current item (index 0 is the outmost, while the
+	 * highest index is the actual parent of the current item)
+	 */
+	public static function activateMenuItems(array &$items, callable $isActiveCallback, bool $activateParents, array $parentItems=[]) : void
+	{
+		foreach ($items as &$item) {
+			$item['active'] = call_user_func($isActiveCallback, $item, $parentItems);
+
+			if ($item['active'] && $activateParents && !empty($parentItems)) {
+				foreach ($parentItems as &$parentItem) {
+					$parentItem['active'] = true;
+				}
+			}
+
+			if (isset($item['items']) && !empty($item['items'])) {
+				static::activateMenuItems($item['items'], $isActiveCallback, $activateParents, array_merge($parentItems, [&$item]));
+			}
+		}
+	}
+
+	/**
+	 * Extracts the active paths from a prepared menu items array as used in the yii menus
+	 *
+	 * @param array $items the items to process
+	 * @return array an array of active paths within the items processed
+	 */
+	public static function activePaths(array &$items) : array
+	{
+		$activePaths = [];
+
+		//call method on active root elements
+		foreach ($items as &$item) {
+			if (isset($item['active']) && $item['active']) {
+				static::getActivePathsRecursive($item,$activePaths);
+			}
+		}
+
+		return $activePaths;
+	}
+
+	/**
+	 * This method is called for each found active item in a recursive way. It will be called again for each
+	 * active child found.
+	 *
+	 * @param array $item the active (!) item to get the path for
+	 * @param array $activePaths the target array containing the active paths
+	 * @param array $currentPath path of the parent items down to the one currently processed (without current item, only parents)
+	 */
+	protected static function getActivePathsRecursive(array &$item, array &$activePaths, array $currentPath=[]) : void
+	{
+		//iterate over children
+		$hasActiveChild = false;
+		if (isset($item['items'])) {
+			foreach ($item['items'] as &$childItem) {
+				if (isset($childItem['active']) && $childItem['active']) {
+					$hasActiveChild = true;
+					static::getActivePathsRecursive($childItem, $activePaths, array_merge($currentPath, [$item]));
+				}
+			}
+		}
+
+		//if last active leave add the path
+		if (!$hasActiveChild) {
+			$activePaths[] = array_merge($currentPath, [$item]);
+		}
 	}
 
 	/**
@@ -96,7 +173,7 @@ class MenuItemHelper
 		}
 
 		//render children
-		$children = static::getChildrenForItem($model, $allItems);
+		$children = static::getChildrenForItem($model);
 		if (count($children) > 0) {
 			$itemArr['items'] = [];
 			foreach ($children as $child) {
@@ -111,20 +188,29 @@ class MenuItemHelper
 	 * Helper method to extract the children of an item from the flat model list
 	 *
 	 * @param \asinfotrack\yii2\article\models\MenuItem $parentItem the item to fetch the children for
-	 * @param \asinfotrack\yii2\article\models\MenuItem[] $allItems an array containing all menu item models
 	 * @return \asinfotrack\yii2\article\models\MenuItem[] the direct descendants of the model
 	 */
-	protected static function getChildrenForItem(MenuItem &$parentItem, array &$allItems) : array
+	protected static function getChildrenForItem(MenuItem &$parentItem) : array
 	{
-		return array_filter($allItems, function ($menuItem) use (&$parentItem) {
+		return array_filter(static::$ITEM_CACHE, function ($menuItem) use (&$parentItem) {
 			/* @var $menuItem \asinfotrack\yii2\article\models\MenuItem|\creocoder\nestedsets\NestedSetsBehavior */
 
+			if ($menuItem->tree != $parentItem->tree) return false;
 			if ($menuItem->depth != $parentItem->depth + 1) return false;
 			if ($menuItem->lft <= $parentItem->lft) return false;
 			if ($menuItem->rgt >= $parentItem->rgt) return false;
 
 			return true;
 		});
+	}
+
+	/**
+	 * Method which caches the menu items
+	 */
+	protected static function cacheMenuItems()
+	{
+		if (static::$ITEM_CACHE !== null) return;
+		static::$ITEM_CACHE = MenuItem::find()->orderTree()->all();
 	}
 
 }
