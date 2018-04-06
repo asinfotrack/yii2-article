27e85465ea3427042ccacb3cd2a7930f5cbdcc98
Leonardo Parrino <leonardo.parrino@asinfotrack.ch>
2018-04-06 09:32:40 +0200

    bug fix article unlink

--- models/Article.php
+++ models/Article.php
@@ -281,7 +281,7 @@ class Article extends \yii\db\ActiveRecord
 		$oldCatIds = ArrayHelper::getColumn($oldCats, 'id');
 		foreach ($oldCats as $cat) {
 			if (!in_array($cat->id, $this->categoryIds)) {
-				$this->unlink('courseCategories', $cat);
+				$this->unlink('articleCategories', $cat);
 			}
 		}
 
